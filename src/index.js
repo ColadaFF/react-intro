10f4a88e4901304901ca6169b5df86fd7c063e95
coladaff <cs3kns@gmail.com>
2019-04-27 10:14:02 -0500

    tools

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.breed),
+    React.createElement("h2", {}, props.animal)
+  ]);
+};
+
+const App = () => {
+  // <h1>Hello World</h1>
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adoptar!"),
+    React.createElement(Pet, { name: "Luna", breed: "Basset", animal: "Dog" }),
+    React.createElement(Pet, { name: "Chester", breed: "Cold", animal: "Cat" })
+  ]);
+};
+
+const RootElement = document.getElementById("root");
+ReactDOM.render(React.createElement(App), RootElement);
